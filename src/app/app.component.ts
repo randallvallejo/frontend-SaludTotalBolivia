a0248fe2512653160fa64a89dd63676a09fa2c19
Matheo Quiroga <matheo.quiroga@ucb.edu.bo>
2025-05-20 23:13:26 -0400

    feat:register part1 view hotfix: styles and routerLinks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,20 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './home/navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { Title } from '@angular/platform-browser';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,NavbarComponent,HomeComponent, LoginComponent],
+  imports: [RouterOutlet,NavbarComponent,HomeComponent, LoginComponent,RegisterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'frontend';
+  constructor(private title: Title) {
+  }
+  ngOnInit() {
+    this.title.setTitle('Home');
+  }
 }
--- src/app/home/navbar/navbar.component.ts
+++ src/app/home/navbar/navbar.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterLink,RouterLinkActive],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
-
+  constructor(private title: Title) {
+  }
+  setTitle(newTitle: string) {
+    this.title.setTitle(newTitle);
+  }
 }
