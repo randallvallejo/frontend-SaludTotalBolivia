f478fd9c7eb7d1d4d11a69cba1df5a1d87c79027
aleloayza <alejandraloayza68@gmail.com>
2025-05-22 10:09:28 -0400

    vista de consultas con imagenes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './home/navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { ConsultasComponent } from './consultas/consultas.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,NavbarComponent,HomeComponent, LoginComponent],
+  imports: [RouterOutlet,NavbarComponent,HomeComponent, LoginComponent,ConsultasComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/consultas/preguntas-frecuentes/preguntas-frecuentes.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-preguntas-frecuentes',
+  imports: [],
+  templateUrl: './preguntas-frecuentes.component.html',
+  styleUrl: './preguntas-frecuentes.component.scss'
+})
+export class PreguntasFrecuentesComponent {
+
+}
