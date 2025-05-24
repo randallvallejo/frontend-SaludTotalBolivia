84d52351ad3b523c6b7704a8a20019b143c05ba9
aleloayza <alejandraloayza68@gmail.com>
2025-05-24 13:25:49 -0400

    seleccionar reserva y confirmar con el boton listo, vista completa

---
+++ src/app/reserva-seleccion/analisis/analisis.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-analisis',
+  imports: [],
+  templateUrl: './analisis.component.html',
+  styleUrl: './analisis.component.scss'
+})
+export class AnalisisComponent {
+
+}
---
+++ src/app/reserva-seleccion/hospitales/hospitales.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hospitales',
+  imports: [],
+  templateUrl: './hospitales.component.html',
+  styleUrl: './hospitales.component.scss'
+})
+export class HospitalesComponent {
+
+}
