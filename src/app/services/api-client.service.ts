dbd3f1a81ef1be187db542d92e343ea203cc0ab4
Randall Vallejo <randall.vallejo@ucb.edu.bo>
2025-05-29 20:02:58 -0400

    Corrige environment.ts con export

--- src/app/services/api-client.service.ts
+++ src/app/services/api-client.service.ts
@@ -8,7 +8,7 @@ import { environment } from '../../environments/environment';
   providedIn: 'root'
 })
 export class ApiClientService {
-  private baseUrl = environment.API_BASE_URL;
+private baseUrl = environment.apiUrl;
   constructor(private http: HttpClient) { }
 
   get(url: string): Observable<any> {
